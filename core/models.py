ce7b527e60236abcb410f8ed8333394312d6433e
Juan Alencar de Barros <127232363+juanalenca@users.noreply.github.com>
2024-10-06 08:24:32 -0300

    Update models.py

--- core/models.py
+++ core/models.py
@@ -1,11 +1,12 @@
 from django.db import models
 
-# Create your models here.
+# Modelo de Produto com campos para nome, preço e estoque
 class Produto(models.Model):
     nome = models.CharField('Nome', max_length=100)
     preco = models.DecimalField('Preço', max_digits=8, decimal_places=2)
     estoque = models.IntegerField('Quantidade de estoque')
-    
+
+# Modelo de Cliente com campos para nome, sobrenome e email
 class cliente(models.Model):
     nome = models.CharField('Nome', max_length=100)
     sobrenome = models.CharField('Sobrenome', max_length=100)
