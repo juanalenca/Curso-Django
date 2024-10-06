9e6ea751efe1d4f1df1b9503170b37dda7803b7f
Juan Alencar de Barros <127232363+juanalenca@users.noreply.github.com>
2024-10-06 08:30:25 -0300

    Update urls.py

--- core/urls.py
+++ core/urls.py
@@ -1,7 +1,8 @@
-from django.urls import path
+from django.urls import path  # Importa a função 'path' para definir as URLs do app
 
-from .views import index, contato
+from .views import index, contato  # Importa as views 'index' e 'contato' do arquivo views.py
 
+# Define a lista de URLs (roteamento do app)
 urlpatterns = [
     path('', index),
     path('contato', contato)
